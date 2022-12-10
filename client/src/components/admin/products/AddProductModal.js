59bb832210c6fe1d4e32b277c1f08df5150f2f48
Nadezzs <nadezcreatesnovels@gmail.com>
2022-12-10 16:38:46 +1100

    first commit

--- client/src/components/admin/products/AddProductModal.js
+++ client/src/components/admin/products/AddProductModal.js
@@ -288,7 +288,7 @@ const AddProductDetail = ({ categories }) => {
                 />
               </div>
               <div className="w-1/2 flex flex-col space-y-1">
-                <label htmlFor="offer">Product Offfer (%) *</label>
+                <label htmlFor="offer">Product Offer (%) *</label>
                 <input
                   value={fData.pOffer}
                   onChange={(e) =>
--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb://localhost:27017/roadx", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
